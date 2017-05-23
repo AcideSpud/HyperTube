9628a4c22dc2834bb9241ca96372fd08b2f86c4b
Julien ROSAMOND <jrosamon@e1r10p5.42.fr>
2017-05-23 17:20:59 +0200

    page_error

--- app.js
+++ app.js
@@ -904,23 +904,19 @@ io.sockets.on('connection', function (socket) {
 
 
 
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get('*', function(req, res, next) {
+  var err = new Error();
+  err.status = 404;
+  next(err);
 });
 
-//app.use(function(req, res, next) {
-  //  var err = new Error('Not Found');
-    //err.status = 404;
-    //next(err);
-//});
+app.use(function(err, req, res, next) {
+  if(err.status !== 404) {
+    return next();
+  }
+  res.status(404);
+  res.render('./pages/error')
+});
 
 
 module.exports = app;
